ef4491236a0a90cab22561b11ccc10dc3a593849
nafis ahamed <nafisahamed14@gmail.com>
2026-08-26 00:30:14 +0600

    first commit

--- backend/src/app/utils/catchAsync.ts
+++
@@ -1,9 +0,0 @@
-import { type NextFunction, type Request, type Response, type RequestHandler } from 'express';
-
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
-  };
-};
-
-export default catchAsync;
